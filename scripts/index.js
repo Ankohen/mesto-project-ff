2c4c6e2df90fc8e42de30378611fcba42c4ee907
Stepan Fisenko <stepkafisenko@yandex.ru>
2024-07-09 00:55:42 +0300

    create all work

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,27 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
 
-// @todo: DOM узлы
+function createCard(cardShowing, deleteCallback) { 
+    const cardElement = cardTemplate
+  .querySelector(".places__item")
+  .cloneNode(true);
+    const deleteButton = cardElement.querySelector(".card__delete-button");
+    const cardImage = cardElement.querySelector(".card__image");
+    const cardTitle = cardElement.querySelector(".card__title");
 
-// @todo: Функция создания карточки
+    cardImage.src = cardShowing.link;
+    cardImage.alt = cardShowing.name;
+    cardTitle.textContent = cardShowing.name;
 
-// @todo: Функция удаления карточки
+    deleteButton.addEventListener('click', deleteCallback)
+    return cardElement;
+}
+function deleteCallback(event) {
+        const target = event.target.closest('.card');
+        target.remove();
+        };
 
-// @todo: Вывести карточки на страницу
+const placesList = document.querySelector('.places__list');
+initialCards.forEach(cardShowing => {
+    const cardElement = createCard(cardShowing, deleteCallback);
+    placesList.appendChild(cardElement);
+  });
